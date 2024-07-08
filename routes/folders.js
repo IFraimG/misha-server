15536f900634d5c86aa27e9cfb6e3e193f9f926e
IFraimG <kulsasha55@gmail.com>
2024-07-08 14:28:54 +0300

    ...

--- routes/folders.js
+++ routes/folders.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const folderController = require("../controllers/folder.js")
 let passport = require("../configs/passportConfig.js")
 
-router.post("/create", folderController.create)
+router.post("/create", passport.authenticate('jwt', { session: false }), folderController.create)
 router.delete("/deleteFolderByFolderID", passport.authenticate('jwt', { session: false }), folderController.deleteFolderByFolderID)
 router.get("/getFolderByFolderID", passport.authenticate('jwt', { session: false }), folderController.getFolderByFolderID)
 router.get("/getFoldersByUserID", passport.authenticate('jwt', { session: false }), folderController.getFoldersByUserID)
