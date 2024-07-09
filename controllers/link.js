8324595903b54fea0eb1905ccc768c1f722a442d
IFraimG <kulsasha55@gmail.com>
2024-07-09 17:04:00 +0300

    ...

--- controllers/link.js
+++ controllers/link.js
@@ -4,6 +4,9 @@ const generateRandomString = require("../utils/generateRandomString.js")
 module.exports.create = async (req, res) => {
     try {
        console.log("body", req.body.body);
+      Object.keys(req.body.body, item => {
+        console.log(item);
+      }) 
         //console.log("title", req.body.title, "description", req.body.description, "folderID", req.body.folderID, "userID", req.body.userID);
         let link = await Link.create({ 
             title: req.body.body.title,
