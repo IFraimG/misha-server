0cc0b0784b24248d42aa72c0709a0d0e3c3e8110
IFraimG <kulsasha55@gmail.com>
2024-07-09 16:45:06 +0300

    ...

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use('/uploads', express.static('uploads'));
-// app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 app.use('/', indexRouter);
 app.use('/auth/users', authUserRouter);
--- controllers/link.js
+++ controllers/link.js
@@ -3,6 +3,7 @@ const generateRandomString = require("../utils/generateRandomString.js")
 
 module.exports.create = async (req, res) => {
     try {
+        console.log("body", req.body.body);
         console.log("title", req.body.title, "description", req.body.description, "folderID", req.body.folderID, "userID", req.body.userID);
         let link = await Link.create({ 
             title: req.body.title,
