c096f1e91ead08717348a426ffbcaee6eb2e8198
IFraimG <kulsasha55@gmail.com>
2024-07-09 14:58:58 +0300

    test2

--- routes/links.js
+++ routes/links.js
@@ -4,7 +4,7 @@ const taskController = require('../controllers/link.js');
 let passport = require("../configs/passportConfig.js")
 let upload = require("../configs/upload.js")
 
-router.post("/create", upload.array("img"), passport.authenticate('jwt', { session: false }), taskController.create)
+router.post("/create", upload.single("img"), passport.authenticate('jwt', { session: false }), taskController.create)
 router.get("/getLinkByID", passport.authenticate('jwt', { session: false }), taskController.getLinkByID)
 router.get("/getLinksByFolderID", passport.authenticate('jwt', { session: false }), taskController.getLinksByFolderID)
 router.delete("/deleteLinkByLinkID", passport.authenticate('jwt', { session: false }), taskController.deleteLinkByLinkID)
